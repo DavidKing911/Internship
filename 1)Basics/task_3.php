bbdcd9265757a0432d17f2b5e9bb8a63a21b2dcf
David <avetyan_david@bk.ru>
2025-03-04 09:43:42 +0700

    Task 1.3 complete

---
+++ 1)Basics/task_3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP - Task 3</h1>
+    <?php
+        $nick = "fdsgdgddhghfg";
+        if (strlen($nick) % 2 == 0) {
+            echo "Girl!";
+        } else {
+            echo "Boy!";
+        }
+    ?>
+</form>
+</body>
+</html>
