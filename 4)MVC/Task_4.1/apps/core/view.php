086e709fa441b252a1dd7b5354bd4aaa7997d1b4
David <avetyan_david@bk.ru>
2025-04-10 13:53:26 +0700

    Beginning of the 4th block

---
+++ 4)MVC/Task_4.1/apps/core/view.php
@@ -0,0 +1,8 @@
+<?php
+class View
+{
+	function generate($content_view)
+	{
+		include 'application/views/'.$content_view;
+	}
+}
---
+++ 4)MVC/Task_4.1/apps/views/main_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MVC</title>
+    </head>
+    <body>
+        <h1>Добро пожаловать!</h1>
+        <p>
+            Если хотите попасть в нашу компанию, то пройдите <a href="/">регистрацию</a>!
+        </p>
+    </body>
+</html>
